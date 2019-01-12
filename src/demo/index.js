13c344898c88b140e386f24a7ded0e716d405adc
Nazmuz Pranto <nazmuz.pranto@endurance.com>
2019-01-12 15:18:21 -0500

    removes source code from demo file

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,12 +1,1 @@
-import { STATES, getTaxRate, calculateTax } from './../lib';
-
-console.log(
-  getTaxRate(STATES.ALABAMA),
-  getTaxRate(STATES.MASSACHUSETTS),
-  getTaxRate(STATES.ARKANSAS),
-  getTaxRate(STATES.DISTRICT_OF_COLUMBIA),
-  getTaxRate(STATES.KENTUCKY),
-  getTaxRate(STATES.NORTH_CAROLINA),
-);
-
-console.log(calculateTax(49.99, STATES.NEW_YORK));
+// Use to test functionalities and values in the browser
